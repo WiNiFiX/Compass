67155d9698856e1ec052b02390383e7969d728be
olesgeras <26541518+olesgeras@users.noreply.github.com>
2021-12-09 17:40:58 +0200

    Tray implementation

--- app/main.js
+++ app/main.js
@@ -1,41 +1,82 @@
-const { app, BrowserWindow, ipcMain, Menu } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, Tray, nativeImage, shell } = require('electron');
 const path = require('path');
-const {runApp} = require('./bot.js');
+const { startApp, stopApp } = require('./bot.js');
+
+let win, tray;
+
+const createGameWindow = () => {
+  return new Promise((resolve, reject) => {
+    win = new BrowserWindow({
+      fullscreen: true,
+      show: false,
+      webPreferences: {
+        contextIsolation: false,
+        nodeIntegration: true
+      },
+      frame: false,
+      transparent: true
+    });
+
+    win.loadFile('./app/index.html');
+    win.setAlwaysOnTop(true);
+    win.setFocusable(false);
+    win.setResizable(false);
+    win.setIgnoreMouseEvents(true);
+    win.removeMenu();
+
+    win.once('ready-to-show', () => {
+      win.show();
+      resolve(win);
+    });
+  });
+};
 
 
-let win;
-const createWindow = exports.createWindow = () => {
-  win = new BrowserWindow({
-    fullscreen: true,
-    show: false,
-    webPreferences: {
-      contextIsolation: false,
-  	  nodeIntegration: true
+const createTray = () => {
+  const icon = nativeImage.createFromPath(path.join(__dirname, '/img/arr.png'));
+  tray = new Tray(icon);
+  const template = [
+    {
+      label: 'Start Compass',
+      click() {
+        createGameWindow()
+        .then(startApp)
+        .catch(e => console.log(e));
+      }
     },
-    frame: false,
-    transparent: true
-  })
+    {
+      label: 'Stop compass',
+      click() {
+        stopApp();
+        win.destroy();
+      }
+    },
+    { type: 'separator'},
+    {
+      label: 'Options'
+    },
+    { type: 'separator'},
+    {
+      label: 'About',
+      click() {
+        shell.openExternal('https://www.youtube.com/olesgeras');
+      }
+    },
+    {
+      label: 'Exit',
+      role: 'quit'
+    }
+  ];
 
-  win.loadFile('./app/index.html')
-  win.setAlwaysOnTop(true);
-  win.setFocusable(false);
-  win.setResizable(false);
-  win.setClosable(false);
-  win.setIgnoreMouseEvents(true);
-  win.removeMenu();
+  tray.setToolTip('lol');
+  tray.setContextMenu(Menu.buildFromTemplate(template));
+};
 
-  win.once('ready-to-show', () => {
-    win.show();
-  })
-}
 
 app.whenReady().then(() => {
-  createWindow();
-  runApp(win);
-  // win.webContents.openDevTools();
+  createTray();
 })
 
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+app.on('window-all-closed', (event) => {
+  event.preventDefault();
 })
