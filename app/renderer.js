11855d34671eb427717d58f889e94a56a2f37ab2
olesgeras <26541518+olesgeras@users.noreply.github.com>
2021-12-11 18:36:16 +0200

    Compass area + options module

--- app/renderer.js
+++ app/renderer.js
@@ -1,32 +1,40 @@
 const { ipcRenderer } = require('electron');
 
+const compassArea = document.querySelector('.compass');
+let options;
+
+ipcRenderer.invoke('get-options')
+.then(newOpts => options = newOpts);
+
+
 const renderEnemy = ({scale, angle}) => {
   let enemy = document.createElement('div');
   enemy.classList.add('enemy');
-  enemy.style.left = `${890 + Math.cos(angle) * 200}px`;
-  enemy.style.top = `${500 + Math.sin(angle) * 200}px`;
+  enemy.style.left = `${890 + Math.cos(angle) * options.radius}px`;
+  enemy.style.top = `${500 + Math.sin(angle) * options.radius}px`;
+
+  enemy.style.width = `${options.size}px`;
+  enemy.style.height = `${options.size}px`;
+  enemy.style.opacity = `${options.opacity / 100}`;
 
   enemy.style.transform = `scale(${scale}) rotate(${angle / (Math.PI * 2)}turn)`;
   return enemy;
 };
 
-const append = (node) => {
-  document.body.append(node);
-}
+(() => {
+  let count = 0;
+  ipcRenderer.on('set-enemies', (event, enemies) => {
 
-let count = 0;
-ipcRenderer.on('set-enemies', (event, enemies) => {
+    if(count == 10) {
+      compassArea.innerHTML = ``;
+      count = 0;
+    };
 
-  if(count == 5) {
-    document.body.innerHTML = ``;
-    count = 0;
-  };
-
-  if(enemies.length > 0) {
-    document.body.innerHTML = ``;
-    enemies.map(renderEnemy).forEach(append);
-  } else {
-    count += 1;
-  }
-
-});
+    if(enemies.length > 0) {
+      compassArea.innerHTML = ``;
+      enemies.map(renderEnemy).forEach(node => compassArea.append(node));
+    } else {
+      count += 1;
+    }
+  });
+})();
